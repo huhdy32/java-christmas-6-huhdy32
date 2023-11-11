ecbedb85f8e08bf8bae1e3ec1958d73a14cb298f
huhdy32 <mong3268@gmail.com>
2023-11-11 13:38:43 +0900

    feat(OutputView): 기본 뼈대 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,47 @@
+package christmas.view;
+
+public class OutputView extends View {
+
+    public void printAllDetails() {
+        this.printEventDate();
+        this.printOrder();
+        this.printOrderAmount();
+        this.printFreeGift();
+        this.printBenefitDetails();
+        this.printTotalBenefitAmount();
+        this.printTotalAmount();
+        this.printEventBadge();
+    }
+
+    private void printEventDate() {
+
+    }
+
+    private void printOrder() {
+
+    }
+
+    private void printOrderAmount() {
+
+    }
+
+    private void printFreeGift() {
+
+    }
+
+    private void printBenefitDetails() {
+
+    }
+
+    private void printTotalBenefitAmount() {
+
+    }
+
+    private void printTotalAmount() {
+
+    }
+
+    private void printEventBadge() {
+
+    }
+}
