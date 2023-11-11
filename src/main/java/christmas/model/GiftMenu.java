7eddd9ec05038a668a6a4cea8c5adb39f419bd31
huhdy32 <mong3268@gmail.com>
2023-11-11 17:33:33 +0900

    feat(GiftMenu): 할인전 금액을 통해 사은품을 얻을 수 있도록 구현

---
+++ src/main/java/christmas/model/GiftMenu.java
@@ -0,0 +1,23 @@
+package christmas.model;
+
+import java.util.Arrays;
+
+public enum GiftMenu {
+
+    샴페인(Menu.샴페인, 120_000),
+    없음(null, 0);
+    private Menu menu;
+    private int minimum_Amount_For_Free_Gift;
+
+    GiftMenu(Menu menu, int minimum_Amount_For_Free_Gift) {
+        this.menu = menu;
+        this.minimum_Amount_For_Free_Gift = minimum_Amount_For_Free_Gift;
+    }
+
+    public static GiftMenu getGift(int amount) {
+        return Arrays.stream(GiftMenu.values())
+                .filter(giftMenu -> giftMenu.minimum_Amount_For_Free_Gift <= amount)
+                .findFirst()
+                .orElse(없음);
+    }
+}
