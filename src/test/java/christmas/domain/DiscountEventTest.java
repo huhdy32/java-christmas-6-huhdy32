7d3c237ea4f72f0ee7beaadd4257717326722c3e
huhdy32 <mong3268@gmail.com>
2023-11-13 02:31:34 +0900

    test: 할인율 적용 테스트 케이스 추가

--- src/test/java/christmas/domain/DiscountEventTest.java
+++ src/test/java/christmas/domain/DiscountEventTest.java
@@ -1,2 +1,86 @@
-package christmas.domain;public class DiscountEventTest {
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class DiscountEventTest {
+
+    @ParameterizedTest
+    @DisplayName("예약 일자를 바탕으로 적용되는 이벤트 확인하기")
+    @MethodSource("provide_Reservation_Date")
+    void 적절한_이벤트_적용_확인(int date, List<DiscountEvent> events) {
+        assertThat(DiscountEvent.getEvents(date))
+                .containsAll(events);
+    }
+
+    @ParameterizedTest
+    @DisplayName("할인 적용 확인")
+    @MethodSource("provide_Menus")
+    void 할인율_적절한지_테스트(int reservationDate, List<Menu> menus, int result) {
+        assertThat(
+                DiscountEvent.getEvents(reservationDate).stream()
+                        .mapToInt(event -> event.getDiscount(reservationDate, menus))
+                        .sum())
+                .isEqualTo(result);
+    }
+
+    static Stream<Arguments> provide_Reservation_Date() {
+        return Stream.of(
+                Arguments.of(
+                        3,
+                        List.of(DiscountEvent.CHRISTMAS_DISCOUNT,
+                                DiscountEvent.WEEKDAY_DISCOUNT,
+                                DiscountEvent.SPECIAL_DISCOUNT)
+                ),
+                Arguments.of(
+                        31,
+                        List.of(DiscountEvent.WEEKDAY_DISCOUNT,
+                                DiscountEvent.SPECIAL_DISCOUNT)),
+                Arguments.of(
+                        26,
+                        List.of(DiscountEvent.WEEKDAY_DISCOUNT)
+                ),
+                Arguments.of(
+                        6,
+                        List.of(DiscountEvent.CHRISTMAS_DISCOUNT,
+                                DiscountEvent.WEEKDAY_DISCOUNT)
+                ),
+                Arguments.of(
+                        22,
+                        List.of(DiscountEvent.WEEKEND_DISCOUNT,
+                                DiscountEvent.CHRISTMAS_DISCOUNT)
+                )
+
+        );
+    }
+    static Stream<Arguments> provide_Menus() {
+        return Stream.of(
+                Arguments.of(
+                        26,
+                        List.of(Menu.아이스크림,
+                                Menu.초코케이크,
+                                Menu.제로콜라),
+                        4046
+                ),
+                Arguments.of(
+                        3,
+                        List.of(Menu.아이스크림,
+                                Menu.초코케이크,
+                                Menu.바비큐립),
+                        6346
+                ),
+                Arguments.of(
+                        22,
+                        List.of(Menu.티본스테이크,
+                                Menu.양송이스프),
+                        5223
+                )
+        );
+    }
 }
--- src/test/java/christmas/domain/GiftMenuTest.java
+++ src/test/java/christmas/domain/GiftMenuTest.java
@@ -1,2 +1,28 @@
-package christmas.domain;public class GiftMenuTest {
+package christmas.domain;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GiftMenuTest {
+    @ParameterizedTest
+    @DisplayName("증정품 제공 테스트")
+    @ValueSource(ints = {120_000, 130_000})
+    void provide_Payment(int totalPayAmount) {
+        assertThat(GiftMenu.getGift(totalPayAmount))
+                .isEqualTo(GiftMenu.샴페인);
+    }
+
+    @ParameterizedTest
+    @DisplayName("없음 처리 테스트")
+    @ValueSource(ints = {1, -12, 0})
+    void provide_Invalid_Payment(int totalPayAmount) {
+        assertThat(GiftMenu.getGift(totalPayAmount))
+                .isEqualTo(GiftMenu.없음);
+    }
+
 }
