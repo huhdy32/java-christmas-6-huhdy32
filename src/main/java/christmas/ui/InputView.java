4feeb8bd861df0e418d7f4b05a518702519e5aa2
huhdy32 <mong3268@gmail.com>
2023-11-13 00:54:16 +0900

    refactor: input, output 인터페이스 삭제

---
+++ src/main/java/christmas/ui/InputView.java
@@ -0,0 +1,16 @@
+package christmas.ui;
+
+import christmas.ui.view.ConsoleView;
+
+public class InputView extends ConsoleView {
+
+    public String getDate() {
+        out("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return in();
+    }
+
+    public String getOrder() {
+        out("주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1");
+        return in();
+    }
+}
---
+++ src/main/java/christmas/ui/OutputView.java
@@ -0,0 +1,59 @@
+package christmas.ui;
+
+import christmas.domain.Reservation;
+import christmas.ui.view.ConsoleView;
+
+public class OutputView extends ConsoleView {
+
+    private Reservation reservation;
+
+    public void printDetails(Reservation reservation) {
+        this.reservation = reservation;
+        printEventDate();
+        printOrders();
+        printOrderAmount();
+        printFreeGift();
+        printBenefitDetails();
+        printTotalBenefitAmount();
+        printTotalAmount();
+        printEventBadge();
+    }
+
+    private void printEventDate() {
+        out("12월 " + reservation.getDate() + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
+    private void printOrders() {
+        out("<주문 메뉴>");
+    }
+
+    private void printOrderAmount() {
+        out("<할인 전 총주문 금액>");
+        out(String.format("%,d", reservation.getTotalOrderAmount()) + "원");
+    }
+
+    private void printFreeGift() {
+        out("<증정 메뉴>");
+        out(reservation.getGift());
+    }
+
+    private void printBenefitDetails() {
+        out("<혜택 내역>");
+    }
+
+    private void printTotalBenefitAmount() {
+        out("<총혜택 금액>");
+    }
+
+    private void printTotalAmount() {
+        out("<할인 후 예상 결제 금액>");
+    }
+
+    private void printEventBadge() {
+        out("<12월 이벤트 배지>");
+    }
+
+    public void printError(String message) {
+        out("[ERROR] " + message);
+    }
+}
