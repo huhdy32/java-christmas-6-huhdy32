1105aa77e1487a82400dd07fb08368b56fb8c2fb
huhdy32 <mong3268@gmail.com>
2023-11-13 01:10:23 +0900

    refactor(InputErrorHandler): 상수 필드 추가

--- src/main/java/christmas/controller/InputErrorHandler.java
+++ src/main/java/christmas/controller/InputErrorHandler.java
@@ -2,41 +2,42 @@ package christmas.controller;
 
 import christmas.domain.Menu;
 import christmas.logic.Logic;
-import christmas.utils.ErrorMessage;
 
 import java.util.List;
 
 public class InputErrorHandler {
+    public static final String ILLEGAL_ORDER = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String ILLEGAL_RESERVATION_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     public void validateMenuName(String name) {
         try {
             Menu.valueOf(name);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_ORDER);
+            throw new IllegalArgumentException(ILLEGAL_ORDER);
         }
     }
 
     public void isDuplicatedOrder(List<Menu> menus, Menu menu) {
         if (menus.contains(menu)) {
-            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_ORDER);
+            throw new IllegalArgumentException(ILLEGAL_ORDER);
         }
     }
 
     public void validateOrderCount(int orderCount) {
         if (orderCount == 0) {
-            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_ORDER);
+            throw new IllegalArgumentException(ILLEGAL_ORDER);
         }
     }
 
     // 1~ 31 사이의 숫자인지 확인
     public void validateDate(int date) {
         if (date > 31 || date < 0) {
-            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_RESERVATION_DATE);
+            throw new IllegalArgumentException(ILLEGAL_RESERVATION_DATE);
         }
     }
     // 주문된 최종 메뉴 형식이 바람직한지 확인
     public void validateMenus(List<Menu> menus) {
         if (validateSize(menus) || validateOrder(menus)) {
-            throw new IllegalArgumentException(ErrorMessage.ILLEGAL_ORDER);
+            throw new IllegalArgumentException(ILLEGAL_ORDER);
         }
     }
 
