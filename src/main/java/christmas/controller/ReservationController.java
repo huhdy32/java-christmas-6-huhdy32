30e73bef8dc773837df03ac74097ba9c7a274e96
huhdy32 <mong3268@gmail.com>
2023-11-14 02:38:19 +0900

    feat: 사용자로부터의 입력을 비즈니스 로직으로 연결하는 클래스 구현

--- src/main/java/christmas/controller/ReservationController.java
+++ src/main/java/christmas/controller/ReservationController.java
@@ -1,2 +1,62 @@
-package christmas.controller;public class ReservationController {
+package christmas.controller;
+
+import christmas.domain.Reservation;
+import christmas.domain.enums.Menu;
+import christmas.service.EventService;
+import christmas.ui.InputView;
+import christmas.ui.OutputView;
+
+import java.util.List;
+
+public class ReservationController {
+    private final ReservationRequestParser reservationRequestParser;
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private EventService eventService;
+
+    public ReservationController(ReservationRequestParser reservationRequestParser, InputView inputView, OutputView outputView) {
+        this.reservationRequestParser = reservationRequestParser;
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void getReservation() {
+        // 예약 일자 입력 받기
+        int date = getReservationDate();
+        // 주문 목록 입력 받기
+        List<Menu> menus = getReservationMenus();
+        try {
+            // 예약 생성
+            Reservation reservation = Reservation.create(menus, date);
+            // 이벤트 적용 목록 출력하기
+            eventService = EventService.create(reservation);
+            outputView.printDetails(eventService);
+        } catch (IllegalStateException e) {
+            outputView.printError(e.getMessage());
+            getReservation();
+        }
+    }
+
+    // 예약 메뉴 요청
+    private List<Menu> getReservationMenus() {
+        try {
+            String reservationOrder = inputView.getOrder();
+            return reservationRequestParser.parseOrders(reservationOrder);
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            return getReservationMenus();
+        }
+    }
+
+    // 예약 일자 요청
+    private int getReservationDate() {
+        try {
+            String reservationDate = inputView.getDate();
+            return reservationRequestParser.parseDate(reservationDate);
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e.getMessage());
+            return getReservationDate();
+        }
+    }
 }
