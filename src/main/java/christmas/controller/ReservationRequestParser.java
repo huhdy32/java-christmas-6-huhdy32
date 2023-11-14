7eed8cea7f9da6f719f65b7ee1ac8260c4504409
huhdy32 <mong3268@gmail.com>
2023-11-14 16:35:00 +0900

    fix(ReservationRequestParser): 중복메뉴 처리를 수행하지 못하는 에러 수정

--- src/main/java/christmas/controller/ReservationRequestParser.java
+++ src/main/java/christmas/controller/ReservationRequestParser.java
@@ -11,15 +11,15 @@ public class ReservationRequestParser {
     public static final String ILLEGAL_RESERVATION_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     private final OutputView outputView;
-    private final Set<Menu> orderedMenu = new HashSet<>();
+    private Set<Menu> orderedMenu = new HashSet<>();
 
     public ReservationRequestParser(OutputView outputView) {
         this.outputView = outputView;
     }
 
     public List<Menu> parseOrders(String orders) {
-        String[] seperatedOrders = orders.split(",");
         List<Menu> menus = new ArrayList<>();
+        String[] seperatedOrders = orders.split(",");
         for (int i = 0; i < seperatedOrders.length; i++) {
             Order order = parseOrder(seperatedOrders[i]);
             menus.addAll(order.getMenus());
@@ -30,7 +30,7 @@ public class ReservationRequestParser {
     private Order parseOrder(String singleOrder) {
         String[] order = singleOrder.split("-");
         if (order.length != 2) {
-            throw new IllegalArgumentException(ILLEGAL_ORDER);
+            throw new IllegalArgumentException(ILLEGAL_ORDER + "주문은 '-'로 구분되야 합니다.");
         }
         String orderName = order[0];
         String orderCount = order[1];
@@ -43,7 +43,7 @@ public class ReservationRequestParser {
         try {
             count = Integer.parseInt(orderCount);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ILLEGAL_ORDER);
+            throw new IllegalArgumentException(ILLEGAL_ORDER + "( 주문수량은 숫자여야 합니다.)");
         }
         return new Order(menu, count);
     }
@@ -64,10 +64,9 @@ public class ReservationRequestParser {
         private int count;
 
         public Order(Menu menu, int count) {
-            validateMenuName(menu);
-
             this.menu = menu;
             this.count = count;
+            validateMenuName(menu);
         }
 
         public List<Menu> getMenus() {
@@ -80,7 +79,7 @@ public class ReservationRequestParser {
 
         private void validateMenuName(Menu menu) {
             if (menu == Menu.NONE || orderedMenu.contains(this.menu)) {
-                throw new IllegalArgumentException(ILLEGAL_ORDER);
+                throw new IllegalArgumentException(ILLEGAL_ORDER + " ( 중복 주문 )");
             }
             orderedMenu.add(this.menu);
         }
