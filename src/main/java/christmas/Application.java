e558f50dd8bd75457f6ba30b6e9054a9baf4bbc0
huhdy32 <mong3268@gmail.com>
2023-11-14 19:54:32 +0900

    feat: 어플리케이션 실행 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,19 @@
 package christmas;
 
+import christmas.controller.ReservationController;
+import christmas.controller.ReservationRequestParser;
+import christmas.ui.InputView;
+import christmas.ui.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        ReservationController reservationController = new ReservationController(
+                new ReservationRequestParser(outputView),
+                inputView,
+                outputView);
+        reservationController.getReservation();
     }
 }
