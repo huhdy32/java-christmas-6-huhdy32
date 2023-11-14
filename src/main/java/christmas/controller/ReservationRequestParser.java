9a6fe95a6bd23680082ebe6461a4b579cec33d7d
huhdy32 <mong3268@gmail.com>
2023-11-14 10:47:23 +0900

    feat: 사용자 입력 파싱 클래스 추가

--- src/main/java/christmas/controller/ReservationRequestParser.java
+++ src/main/java/christmas/controller/ReservationRequestParser.java
@@ -1,67 +1,88 @@
 package christmas.controller;
 
+import christmas.domain.Reservation;
 import christmas.domain.enums.Menu;
-import christmas.ui.InputView;
 import christmas.ui.OutputView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
-public class InputParser {
+public class ReservationRequestParser {
+    public static final String ILLEGAL_ORDER = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String ILLEGAL_RESERVATION_DATE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
-    private final InputErrorHandler inputErrorHandler = new InputErrorHandler();
-    private List<Menu> menus;
-    private final InputView inputView;
     private final OutputView outputView;
+    private final Set<Menu> orderedMenu = new HashSet<>();
 
-    public InputParser(InputView inputView, OutputView outputView) {
-        this.inputView = inputView;
+    public ReservationRequestParser(OutputView outputView) {
         this.outputView = outputView;
     }
 
-    public List<Menu> parseReservationOrder() {
-        menus = new ArrayList<>();;
-        try {
-            String orders = inputView.getOrder();
-            Arrays.stream(parseToSingleOrder(orders))
-                    .forEach(order -> addSingleOrder(order));
-        } catch (IllegalArgumentException e) {
-            outputView.printError(e.getMessage());
-            return parseReservationOrder();
+    public List<Menu> parseOrders(String orders) {
+        String[] seperatedOrders = orders.split(",");
+        List<Menu> menus = new ArrayList<>();
+        for (int i = 0; i < seperatedOrders.length; i++) {
+            Order order = parseOrder(seperatedOrders[i]);
+            menus.addAll(order.getMenus());
         }
-        return menus;
+        return Reservation.validateMenus(menus);
     }
 
-    public int getReservationDate() {
-        String inputDate = inputView.getDate();
+    private Order parseOrder(String singleOrder) {
+        String[] order = singleOrder.split("-");
+        if (order.length != 2) {
+            throw new IllegalArgumentException(ILLEGAL_ORDER);
+        }
+        String orderName = order[0];
+        String orderCount = order[1];
+        return generateOrder(orderName, orderCount);
+    }
+
+    private Order generateOrder(String orderMenu, String orderCount) {
+        Menu menu = Menu.getMenu(orderMenu);
+        int count;
+        try {
+            count = Integer.parseInt(orderCount);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ILLEGAL_ORDER);
+        }
+        return new Order(menu, count);
+    }
+
+    public int parseDate(String inputDate) {
         int reservationDate;
         try {
             reservationDate = Integer.parseInt(inputDate);
+            return Reservation.validateDate(reservationDate);
         } catch (NumberFormatException e) {
-            return getReservationDate();
-        }
-        return reservationDate;
-    }
-
-    private String[] parseToSingleOrder(String orders) {
-        return orders.split(",");
-    }
-
-    private void addSingleOrder(String singleOrder) {
-        String[] order = singleOrder.split("-");
-        int orderCount = Integer.parseInt(order[1]);
-        inputErrorHandler.validateOrderCount(orderCount);
-        addMenu(order[0], orderCount);
-    }
-
-    private void addMenu(String name, int orderCount) {
-        inputErrorHandler.validateMenuName(name);
-        Menu menu = Menu.valueOf(name);
-        inputErrorHandler.isDuplicatedOrder(menus, menu);
-        for (int i = 0; i < orderCount; i++) {
-            menus.add(menu);
+            throw new IllegalArgumentException(ILLEGAL_RESERVATION_DATE);
         }
     }
 
+    protected class Order {
+
+        private Menu menu;
+        private int count;
+
+        public Order(Menu menu, int count) {
+            validateMenuName(menu);
+
+            this.menu = menu;
+            this.count = count;
+        }
+
+        public List<Menu> getMenus() {
+            List<Menu> menus = new ArrayList<>();
+            for (int i = 0; i < count; i++) {
+                menus.add(menu);
+            }
+            return menus;
+        }
+
+        private void validateMenuName(Menu menu) {
+            if (menu == Menu.NONE || orderedMenu.contains(this.menu)) {
+                throw new IllegalArgumentException(ILLEGAL_ORDER);
+            }
+            orderedMenu.add(this.menu);
+        }
+    }
 }
