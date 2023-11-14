0ff036921045ad4729bad6bf38b1e94766de42c3
huhdy32 <mong3268@gmail.com>
2023-11-14 16:41:07 +0900

    refactor(OutputView): dto로부터 데이터를 가져오도록 수정

--- src/main/java/christmas/ui/OutputView.java
+++ src/main/java/christmas/ui/OutputView.java
@@ -1,16 +1,21 @@
 package christmas.ui;
 
 import christmas.domain.Reservation;
+import christmas.domain.enums.Menu;
+import christmas.dto.EventProcessedDto;
 import christmas.ui.view.ConsoleView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class OutputView extends ConsoleView {
 
-    private Reservation reservation;
-
-    public void printDetails(Reservation reservation) {
-        this.reservation = reservation;
-        printEventDate();
-        printOrders();
+    private EventProcessedDto eventProcessedDto;
+    public void printDetails(EventProcessedDto eventProcessedDto) {
+        this.eventProcessedDto = eventProcessedDto;
+        printReservationInfo();
         printOrderAmount();
         printFreeGift();
         printBenefitDetails();
@@ -19,40 +24,50 @@ public class OutputView extends ConsoleView {
         printEventBadge();
     }
 
-    private void printEventDate() {
+    private void printReservationInfo() {
+        Reservation reservation = eventProcessedDto.reservation();
         out("12월 " + reservation.getDate() + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
-    }
-
-    private void printOrders() {
+        out("");
+        Map<Menu, Long> itemCounts = reservation.getMenus().stream()
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
         out("<주문 메뉴>");
+        itemCounts.forEach((menu, count) -> out(menu.getMenuName() + " " + itemCounts.get(menu) + "개"));
     }
 
     private void printOrderAmount() {
         out("<할인 전 총주문 금액>");
-        out(String.format("%,d", reservation.getTotalOrderAmount()) + "원");
+        out(parseWonFormat(eventProcessedDto.totalOrderAmount()));
     }
 
     private void printFreeGift() {
         out("<증정 메뉴>");
-        out(reservation.getGift());
+        out(eventProcessedDto.gifts().getName());
     }
 
     private void printBenefitDetails() {
+        Reservation reservation = eventProcessedDto.reservation();
+        int date = reservation.getDate();
+        List<Menu> menus = reservation.getMenus();
+
         out("<혜택 내역>");
+        eventProcessedDto.discountEvents().stream()
+                .forEach(discountEvent ->
+                        out(discountEvent.name() + " : " + parseWonFormat(discountEvent.getDiscount(date, menus))));
     }
 
     private void printTotalBenefitAmount() {
         out("<총혜택 금액>");
-        out(parseWonFormat(reservation.getTotalBenefitAmount()));
+        out(parseWonFormat(eventProcessedDto.totalBenefitAmount()));
     }
 
     private void printTotalAmount() {
         out("<할인 후 예상 결제 금액>");
-        out(parseWonFormat(reservation.getTotalPayAmount()));
+        out(parseWonFormat(eventProcessedDto.getExpectedPaymentAmount()));
     }
 
     private void printEventBadge() {
         out("<12월 이벤트 배지>");
+        out(eventProcessedDto.badge().getName());
     }
 
     public void printError(String message) {
@@ -60,6 +75,6 @@ public class OutputView extends ConsoleView {
     }
 
     private String parseWonFormat(int amount) {
-        return String.format("%,d", amount);
+        return String.format("%,d", amount) + "원";
     }
 }
